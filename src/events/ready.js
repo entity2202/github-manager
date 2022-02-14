ef946843a7be18ffa279fb19e53995660292ad30
Korrumz2PL <korrumz2@gmail.com>
2022-02-14 16:42:41 +0100

    test8

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,4 +1,17 @@
+const fetch = require("node-fetch")
+const express = require("express")
+const { webhook } = require("../config.json")
+const port = 7777
+let app = express();
 module.exports = (client) => {
     console.log(`Logged in as ${client.user.tag}`);
     client.user.setActivity('github.com/Skyndalex', { type: "WATCHING" });
+
+    app.get('/', (req, res) => {
+        res.send("H")
+    })
+
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+    })
 }
