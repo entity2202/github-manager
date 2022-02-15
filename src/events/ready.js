59f91026d8aa12a512def9cc7c329325c85d66dd
Korrumz2PL <korrumz2@gmail.com>
2022-02-15 19:05:23 +0100

    update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,11 +1,4 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const port = 7777;
-const { MessageEmbed } = require("discord.js");
-app.use(bodyParser.json());
-
-module.exports = (client) => {
+module.exports = client => {
     console.log(`Logged in as ${client.user.tag}`);
     client.user.setActivity('github.com/Skyndalex', { type: "WATCHING" });
 }
