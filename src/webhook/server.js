a7f76fa32468f59372d31a29fae8604491e3fc14
Korrumz2PL <korrumz2@gmail.com>
2022-02-16 19:26:38 +0100

    fixes, pull requests

--- src/webhook/server.js
+++ src/webhook/server.js
@@ -6,74 +6,49 @@ const { MessageEmbed } = require("discord.js");
 app.use(bodyParser.json());
 
 module.exports = (client) => {
-    app.post('/payload2', (req, res) => {
+    app.post('/payload2', async (req, res) => {
         const { body } = req;
+        const { commits, head_commit, pusher, pull_request, issue, repository, after } = body;
         console.log(body);
 
         const channelID = "943261043415720026";
 
-        // Commits
-        if (body.head_commit) {
+        if (commits) {
             let commitList = [];
-            for (let i in body.commits) {
-                commitList.push(body.commits[i].message)
+            for (let i in commits) {
+                commits.push(commits[i].message)
             };
 
             let commit = new MessageEmbed()
-                .setTitle(`(${body.repository.full_name}) New commits [${commitList.length}]`)
-                .setDescription(`Changes, full informations: ${body.head_commit.url}`)
-                .addField(`Message(s)`, `\`${commitList.join(",\n")}\``, true)
-                .addField(`Author`, `\`${body.head_commit.author.username}\``, true)
-                .addField(`Modified file(s)`, `\`${body.head_commit.modified.join(",\n")}\``, true)
+                .setTitle(`(${repository.full_name}) New commits [${commitList.length}]`)
+                .setDescription(`Changes, full informations: https://github.com/Skyndalex/github-manager/commit/${after}`)
+                .addField(`Message(s)`, `\`${commitList.join(",\n") || "None"}\``, true)
+                .addField(`Author`, `\`${pusher.name}\``, true)
+                .addField(`Modified file(s)`, `\`${head_commit.modified.join(",\n") || "None"}\``, true)
                 .setColor(`GREEN`)
-            client.channels.cache.get(channelID).send({ embeds: [commit] })
-            // Issues: Opened
-        } else if (body.action === "opened") {
-            let issue = new MessageEmbed()
-                .setTitle(`(${body.repository.full_name}) Issue opened!`)
-                .setDescription(`${body.issue.body}`)
-                .addField(`Action`, `\`${body.action}\``, true)
-                .addField(`Title`, `\`${body.issue.title}\``, true)
-                .addField(`Opened issues`, `\`${body.issue.number}\``, true)
-                .addField(`Author`, `\`${body.issue.user.login}\``, true)
+            await client.channels.cache.get(channelID).send({embeds: [commit]})
+        } else if (issue?.state === "open") {
+            let issueEmbed = new MessageEmbed()
+                .setTitle(`(${repository.full_name}) Issue opened!`)
+                .setDescription(`${issue.body}`)
+                .addField(`Title`, `\`${issue.title}\``, true)
+                .addField(`Opened issues`, `\`${issue.number}\``, true)
+                .addField(`Author`, `\`${issue.user.login}\``, true)
                 .setColor(`GREEN`)
-            client.channels.cache.get(channelID).send({ embeds: [issue] })
-
-            // Issues: Closed
-        } else if (body.action === "closed") {
+            await client.channels.cache.get(channelID).send({ embeds: [issueEmbed] })
+        } else if (issue?.state === "closed") {
             let issueClosed = new MessageEmbed()
-                .setTitle(`(${body.repository.full_name}) Issue closed: #${body.issue.title}`)
+                .setTitle(`(${body.repository.full_name}) Issue closed: #${issue.title}`)
                 .setColor(`RED`)
-            client.channels.cache.get(channelID).send({ embeds: [issueClosed] })
-            // Forks
-        } else if (body.forkee) {
-            let fork = new MessageEmbed()
-                .setTitle(`New fork/Pull request!`)
-                .addField(`Author`, `\`${body.forkee.owner.login}\``)
-                .addField(`Repo name`, `\`${body.forkee.full_name}\``)
+            await client.channels.cache.get(channelID).send({embeds: [issueClosed]})
+        } else if (pull_request?.state === "open") {
+            let pulLRequestNew = new MessageEmbed()
+                .setTitle(`(${repository.full_name}) New pull request #${pull_request.title} by ${pull_request.user.login}`)
+                .setURL(pull_request.html_url)
                 .setColor(`GREEN`)
-            client.channels.cache.get(channelID).send({ embeds: [fork] })
-        } else if (body.project.state === "open") {
-            let projectOpen = new MessageEmbed()
-                .setTitle(`Project opened!`)
-                .addField(`Name`, `${body.project.name}`)
-                .addField(`Description`, `${body.project.body}`)
-                .addField(`Opened by`, `${body.project.creator.login}`)
-                .addField(`Number`, `${body.project.number}`)
-                .setColor(`GREEN`)
-            client.channels.cache.get(channelID).send({ embeds: [projectOpen] })
-        } else if (body.project.state === "closed") {
-            let projectClosed = new MessageEmbed()
-                .setTitle(`Project closed!`)
-                .addField(`Name`, `${body.project.name}`)
-                .addField(`Description`, `${body.project.body}`)
-                .addField(`Closed by`, `${body.project.creator.login}`)
-                .addField(`Number`, `${body.project.number}`)
-                .setColor(`RED`)
-            client.channels.cache.get(channelID).send({ embeds: [projectClosed] })
+            await client.channels.cache.get(channelID).send({embeds: [pulLRequestNew]})
         }
     })
-
     app.get('/test', (req, res) => res.send('Hello World!'))
 
     app.listen(port, () => console.log(`App listening at http://localhost:${ port }`))
